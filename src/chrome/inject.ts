d84f9a15d6d4810619c374ae2d6039dca4fc3b69
Ahmed Ibrahim <ib153507@gmail.com>
2022-12-15 07:32:31 +0200

    Take a snapshot for the youtube video when pressing ctrl+i

---
+++ src/chrome/inject.ts
@@ -0,0 +1,40 @@
+// Todo: Refractor this function again
+// Add validation and error handling `alert(...errors)`
+// chosoe a better name for the snapshot
+// Stop the video
+function snapshot() {
+  const canvas = document.createElement("canvas");
+  const video = document.querySelector<HTMLVideoElement>("video");
+  const ctx = canvas.getContext("2d");
+
+  if (!video || !ctx) return;
+
+  canvas.width = video.offsetWidth;
+  canvas.height = video.offsetHeight;
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  const base64ImageData = canvas.toDataURL("image/jpeg");
+  const filename =
+    "snap-" +
+    canvas.width +
+    "x" +
+    canvas.height +
+    "-" +
+    video.currentTime +
+    ".jpg";
+
+  const a = document.createElement("a");
+  a.download = filename;
+  a.href = base64ImageData;
+  const img = document.createElement("img");
+  img.src = base64ImageData;
+  img.alt = filename;
+  img.title = "Click to save " + filename;
+  window.open()?.document.body.appendChild(a).appendChild(img);
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey === true && event.key === "i") snapshot();
+});
+
+export {};
